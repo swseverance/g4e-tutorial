a979ba23d501e3e82c610999125fcacaad410e56
Hristo Ivanov <47400757+arjunah@users.noreply.github.com>
2020-01-09 18:36:21 +0200

    WIP Frame buttons

--- app/portfolio.js
+++ app/portfolio.js
@@ -1,6 +1,7 @@
 const RestServerUrl = "http://localhost:22910/";
 const RestServerEndpoint = "GetDemoPortfolio";
 const StreamName = "T42.MarketStream.Subscribe";
+const portfolioTabGroupID = "PortfolioTabs";
 
 let detachedTabs = [];
 let subscriptions = [];
@@ -133,7 +134,7 @@ const setUpAppContent = () => {
             }).catch(error => {
                 console.error(error.message);
             });
-        
+
         document.getElementById("title").textContent = portfolioContext.party.preferredName;
         loadPortfolio(portfolioContext.party.pId);
         partyObj = portfolioContext.party;
@@ -151,11 +152,11 @@ const registerInteropMethod = () => {
     // assign the received party object to partyObj, because we will need it later on.
 
     glue.interop.register({
-            name: "SetParty",
-            displayName: "Set Party",
-            description: "Updates the application window to work with the specified party.",
-            accepts: "composite party"
-        },
+        name: "SetParty",
+        displayName: "Set Party",
+        description: "Updates the application window to work with the specified party.",
+        accepts: "composite party"
+    },
         (args) => {
             loadPortfolio(args.party.pId);
             partyObj = args.party;
@@ -538,53 +539,165 @@ const setUpTabControls = () => {
     // TUTOR_TODO Chapter 4.4 Task 5
     // Check if the window is tabbed - if it isn't, simply return, because in this case we do not need any tab controls.
 
-    // TUTOR_TODO Chapter 4.4 Task 6
-    // We have prepared for you the config objects for both buttons.
-    // const gatherTab = {
-    //     buttonId: "gatherTabs",
-    //     tooltip: "Gather all tabs",
-    //     order: 0,
-    //     imageBase64: "iVBORw0KGgoAAAANSUhEUgAAAIAAAAB6CAYAAAB3N1u0AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAACxIAAAsSAdLdfvwAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuOWwzfk4AAAOlSURBVHhe7d1NaxNRFMbxfpoL7vyA+QpuXfkKVRdKdSWodKMg7tyIUIQ2xdF2TK2TmdS+kFLP5J5FkKo3sXNy5zzPD4YsPWfu35dpY7pm5eLiIhRF8XQ4HG4iX/t7ew/ae6G3xb92WbkGP4+OjuSVooHeHt9k0XB+fn4n7kxz/AcgS4azs7NHcV/6je8AZMFwfHz8LO5Kl/AbgCwXJpPJq7gn/YHPAGSxMB6P38Ud6S/8BSBLhaqqPsb96B98BSALtb/zv8bdKIGPAGSR2TO+POLXs7Wu0Liqdna2t9/05arr+ruOnqL/AcgS7eHfnq3TjV7dpHK/vK9zp+h3ALJAmE6nD+MunWEAOZLhw8nJyUbco1MMIDcyeJC/71/EHTrHAHIiQwf5h87bOL8JBpALGbh9xv8QZzfDAHIgw7bP+LtxblMMYJVkyNkzftM0P2Yj22MAqyIDdv2Mn4IBrIIM1z7jr8c5V4oBWJPBwunp6ZM448oxAEsyVPuM/zzOlwUGYEUGap/xX8fZssEALMgwYVxV7+NcWWEAFuTwt3Wo3DAACzpQjhiABR0oRwzAgg6UIwZgQQfKEQOwoAPliAFY0IFyxAAs6EA5YgAWdKAcMQALOlCOGIAFHSgrk6ap5KVXn6KBEMAtuQZGV+8+QmU0Gt2VuVP1MoBe/ZFsTe7P7C10iVc+gcswqRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRjAnIODg2uHh4c3r/KaTCY35D7bfp8gnm0SBjBnNBp19d/nbe+z/qIpGMAcBgCOAYBjAOAYADgGAI4BgGMA4BgAOAYAjgGAYwDgGAA4BgCOAYBjAOAYADgGAI4BgGMA4BgAOAYAjgGAYwDgGEBmZL5FPqXrv6+maTbltQuWH8c3cBGAzNb+bONPcUxahJcA2pppCQwAHAMAxwDAMQBwDAAcAwDHAMAxAHAMABwDAMcAwDEAcAwAHAMAxwDAMQBwDACclwBCXdf7cUxahIsAWjIf3xS6xOUmAGt8Wzg4BgCOAYBjAOAYADgGAI4BgGMA4BgAOAYAjgGAYwDgGAA4BgAOMQDT71MbXwv/yHbEANz6Vpb39HYkYwCOFJ+LDb0dyRiAIwwAHAMAxwDAMQBwDAAcAwD3pSge6+1I5imAy74yhnYt/JXAra2t67vD3ZdXeZVlub7MLMtbW/sFpubVd+W6GFMAAAAASUVORK5CYII="
-    // };
+    const thisPortfolioWindow = glue.windows.my();
+    const clientWindow = glue.windows.findById(thisPortfolioWindow.context.ownerWindowID);
 
-    // const extractTab = {
-    //     buttonId: "extractTabs",
-    //     tooltip: "Extract all tabs",
-    //     order: 0,
-    //     imageBase64: "iVBORw0KGgoAAAANSUhEUgAAAIAAAABjCAYAAABT7gjnAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyZpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuNi1jMDY3IDc5LjE1Nzc0NywgMjAxNS8wMy8zMC0yMzo0MDo0MiAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RSZWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZVJlZiMiIHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENDIDIwMTUgKFdpbmRvd3MpIiB4bXBNTTpJbnN0YW5jZUlEPSJ4bXAuaWlkOjM2NDE2QTIxQTkyODExRTY4QUY2RTYzRjNEM0ZFRTJBIiB4bXBNTTpEb2N1bWVudElEPSJ4bXAuZGlkOjM2NDE2QTIyQTkyODExRTY4QUY2RTYzRjNEM0ZFRTJBIj4gPHhtcE1NOkRlcml2ZWRGcm9tIHN0UmVmOmluc3RhbmNlSUQ9InhtcC5paWQ6MzY0MTZBMUZBOTI4MTFFNjhBRjZFNjNGM0QzRkVFMkEiIHN0UmVmOmRvY3VtZW50SUQ9InhtcC5kaWQ6MzY0MTZBMjBBOTI4MTFFNjhBRjZFNjNGM0QzRkVFMkEiLz4gPC9yZGY6RGVzY3JpcHRpb24+IDwvcmRmOlJERj4gPC94OnhtcG1ldGE+IDw/eHBhY2tldCBlbmQ9InIiPz7pl9TZAAAEm0lEQVR42uydW08aQRiGnXHDIRViaClNuCgJralpjIXEi/4B/7fXpmurpiSgiRdthdBKcQ2nwk4HgxWJWA6z7Hwz73vljbvMvM93GlaXCSHWIOW621Sm+wfl8CoA9++jSozBAACQEQAAIAAA1kMgAAAkAAAkAAAUekkAAJZnAwBgOQQAwPKSAAAszwYAwHIIAIDlJQEAWJ4NAIDlEDiTN/E8r3RxcdHpdrtRnQC5urqq7e/vf5Q/RgGBuucM2N1X19L4r61WK5/JZCK6rVqa39/Y2PgeiURyJFwSwmdSAd9GyfVvI7xer7uJRGJbR/Nd1/2eSqX6VMynVhLYYDD4xTlP6bjCk5OTo52dnSI1U4ZPBK0gAyjJBkxo+lBgrVY7khmpqFu0jfZrxf4GB4F2APi+v9br9c5jsdgbHczW0WmVEGg1BjYajb7c81oI5ouR3/6D3aRl/r91kARAjp7tzc3N9vr6+qvVNuz3phM0fOkGUQsAzs/Pb3K53DDdJlcYJcIw0xeCIHQAyuWyl8/nhwdSz3RLj4ZAILRtAmXab8nIH0IYpzAzExfTCoBqtfpHjnk9mYE3YHx4EIQCwHDUk/rJOU/D/HBBcOb9zWazOXAc52yZu8fj8ecBmi9G8ztsfjo42EIAdDqdgUzd73TufBncnxkCbtiCoDn3jMN8u8VhvoVhf9/3ky8BMH+BPRv1SIx6BoD5CrI+h/l2zv+UAYD589X7Jx8k5YQXBM0S8uy2z5t6LuJQ8x9nPMulfMoZAJGvIOWTBQCpX03Kp1oCQkv9I+7E2AazWWAN6fuIue/pEIn+tSD3887kCdPYyPBFLulTMJ8KAMqjfyJCyX55OLaOhRdgzV8Hj7cQY3WS9Egxb72nCIBQESVjYU7e9GVTvjUZYMx4borp84541HuApaJ/ZLyJI55ScQK0L5IaTTwuDGRNumaARTt/U8+JA1sXxybZU++NA2BVm0R1xKNaAsSs5pvY6K0aaE418k0zP6xsxjXciFnMR8o3FID/dv/Lnn2jgaVfAmC+qQDMcPiDI13DpwBmQ+Tr1MByzTZmWrSYlPa1WotOAIgnmj6kfEtKgJGpX+czC90zgAmpX+s1aAPAIwMAIxzxjMoaHA03DY2erVOAiSMfAIAAwCwtQKFQ6CL6Z9LAOABkA9g8ODjowfyn1ev1LlzXrRoHgOz/oslkMgqLp2v4XqdIJPI6m82+MLEEDP9ZNACYouPj48/pdLpg9BgIPaquTPn1YrGYNf4cAHoo3/drl5eXIkjzMQZqKs/zTjnnL2W9D7wsAgDNVC6XPyUSife2nQNAcr4/PDysbG1trfRdiegBNJAQ4nelUrnZ29tb+bsSAUDI6nQ6ZxKApIz8UA7BAEDIaT8Wi4VahtEDhDzthf0BAIDlAgAAAAIAEACA7NTcY2Amk4mUSqUGtk6Jbra3tzdJATBU2B8aQgmAAAAEACAAAAEAq6T6zTkAgJiur687AMBieZ73TeX1GN7GRUv9fv+H4zhZlRkAf45FRF+kVJqPEkBIruue7u7uflB9XTZRAVAONFS73T6Lx+Nvg7j2XwEGACbouxHl2VRVAAAAAElFTkSuQmCC"
-    // }
+    if (thisPortfolioWindow.mode !== "tab") {
+        return;
+    } else {
+        // TUTOR_TODO Chapter 4.4 Task 6
+        // We have prepared for you the config objects for both buttons.
+        const gatherTabsBtn = {
+            buttonId: "gatherTabs",
+            tooltip: "Gather all tabs",
+            order: 0,
+            imageBase64: "iVBORw0KGgoAAAANSUhEUgAAAIAAAAB6CAYAAAB3N1u0AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAACxIAAAsSAdLdfvwAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuOWwzfk4AAAOlSURBVHhe7d1NaxNRFMbxfpoL7vyA+QpuXfkKVRdKdSWodKMg7tyIUIQ2xdF2TK2TmdS+kFLP5J5FkKo3sXNy5zzPD4YsPWfu35dpY7pm5eLiIhRF8XQ4HG4iX/t7ew/ae6G3xb92WbkGP4+OjuSVooHeHt9k0XB+fn4n7kxz/AcgS4azs7NHcV/6je8AZMFwfHz8LO5Kl/AbgCwXJpPJq7gn/YHPAGSxMB6P38Ud6S/8BSBLhaqqPsb96B98BSALtb/zv8bdKIGPAGSR2TO+POLXs7Wu0Liqdna2t9/05arr+ruOnqL/AcgS7eHfnq3TjV7dpHK/vK9zp+h3ALJAmE6nD+MunWEAOZLhw8nJyUbco1MMIDcyeJC/71/EHTrHAHIiQwf5h87bOL8JBpALGbh9xv8QZzfDAHIgw7bP+LtxblMMYJVkyNkzftM0P2Yj22MAqyIDdv2Mn4IBrIIM1z7jr8c5V4oBWJPBwunp6ZM448oxAEsyVPuM/zzOlwUGYEUGap/xX8fZssEALMgwYVxV7+NcWWEAFuTwt3Wo3DAACzpQjhiABR0oRwzAgg6UIwZgQQfKEQOwoAPliAFY0IFyxAAs6EA5YgAWdKAcMQALOlCOGIAFHSgrk6ap5KVXn6KBEMAtuQZGV+8+QmU0Gt2VuVP1MoBe/ZFsTe7P7C10iVc+gcswqRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRiAR3q4KRjAnIODg2uHh4c3r/KaTCY35D7bfp8gnm0SBjBnNBp19d/nbe+z/qIpGMAcBgCOAYBjAOAYADgGAI4BgGMA4BgAOAYAjgGAYwDgGAA4BgCOAYBjAOAYADgGAI4BgGMA4BgAOAYAjgGAYwDgGEBmZL5FPqXrv6+maTbltQuWH8c3cBGAzNb+bONPcUxahJcA2pppCQwAHAMAxwDAMQBwDAAcAwDHAMAxAHAMABwDAMcAwDEAcAwAHAMAxwDAMQBwDACclwBCXdf7cUxahIsAWjIf3xS6xOUmAGt8Wzg4BgCOAYBjAOAYADgGAI4BgGMA4BgAOAYAjgGAYwDgGAA4BgAOMQDT71MbXwv/yHbEANz6Vpb39HYkYwCOFJ+LDb0dyRiAIwwAHAMAxwDAMQBwDAAcAwD3pSge6+1I5imAy74yhnYt/JXAra2t67vD3ZdXeZVlub7MLMtbW/sFpubVd+W6GFMAAAAASUVORK5CYII="
+        };
 
-    // TUTOR_TODO Chapter 4.4 Task 7
-    // Implement the frame button click events:
-    // - Which button was clicked?
-    // - How are we going to remember the tabs we detached?
-    // - glue.windows.findById() will be quite helpful here
+        const extractTabsBtn = {
+            buttonId: "extractTabs",
+            tooltip: "Extract all tabs",
+            order: 0,
+            imageBase64: "iVBORw0KGgoAAAANSUhEUgAAAIAAAABjCAYAAABT7gjnAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyZpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuNi1jMDY3IDc5LjE1Nzc0NywgMjAxNS8wMy8zMC0yMzo0MDo0MiAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RSZWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZVJlZiMiIHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENDIDIwMTUgKFdpbmRvd3MpIiB4bXBNTTpJbnN0YW5jZUlEPSJ4bXAuaWlkOjM2NDE2QTIxQTkyODExRTY4QUY2RTYzRjNEM0ZFRTJBIiB4bXBNTTpEb2N1bWVudElEPSJ4bXAuZGlkOjM2NDE2QTIyQTkyODExRTY4QUY2RTYzRjNEM0ZFRTJBIj4gPHhtcE1NOkRlcml2ZWRGcm9tIHN0UmVmOmluc3RhbmNlSUQ9InhtcC5paWQ6MzY0MTZBMUZBOTI4MTFFNjhBRjZFNjNGM0QzRkVFMkEiIHN0UmVmOmRvY3VtZW50SUQ9InhtcC5kaWQ6MzY0MTZBMjBBOTI4MTFFNjhBRjZFNjNGM0QzRkVFMkEiLz4gPC9yZGY6RGVzY3JpcHRpb24+IDwvcmRmOlJERj4gPC94OnhtcG1ldGE+IDw/eHBhY2tldCBlbmQ9InIiPz7pl9TZAAAEm0lEQVR42uydW08aQRiGnXHDIRViaClNuCgJralpjIXEi/4B/7fXpmurpiSgiRdthdBKcQ2nwk4HgxWJWA6z7Hwz73vljbvMvM93GlaXCSHWIOW621Sm+wfl8CoA9++jSozBAACQEQAAIAAA1kMgAAAkAAAkAAAUekkAAJZnAwBgOQQAwPKSAAAszwYAwHIIAIDlJQEAWJ4NAIDlEDiTN/E8r3RxcdHpdrtRnQC5urqq7e/vf5Q/RgGBuucM2N1X19L4r61WK5/JZCK6rVqa39/Y2PgeiURyJFwSwmdSAd9GyfVvI7xer7uJRGJbR/Nd1/2eSqX6VMynVhLYYDD4xTlP6bjCk5OTo52dnSI1U4ZPBK0gAyjJBkxo+lBgrVY7khmpqFu0jfZrxf4GB4F2APi+v9br9c5jsdgbHczW0WmVEGg1BjYajb7c81oI5ouR3/6D3aRl/r91kARAjp7tzc3N9vr6+qvVNuz3phM0fOkGUQsAzs/Pb3K53DDdJlcYJcIw0xeCIHQAyuWyl8/nhwdSz3RLj4ZAILRtAmXab8nIH0IYpzAzExfTCoBqtfpHjnk9mYE3YHx4EIQCwHDUk/rJOU/D/HBBcOb9zWazOXAc52yZu8fj8ecBmi9G8ztsfjo42EIAdDqdgUzd73TufBncnxkCbtiCoDn3jMN8u8VhvoVhf9/3ky8BMH+BPRv1SIx6BoD5CrI+h/l2zv+UAYD589X7Jx8k5YQXBM0S8uy2z5t6LuJQ8x9nPMulfMoZAJGvIOWTBQCpX03Kp1oCQkv9I+7E2AazWWAN6fuIue/pEIn+tSD3887kCdPYyPBFLulTMJ8KAMqjfyJCyX55OLaOhRdgzV8Hj7cQY3WS9Egxb72nCIBQESVjYU7e9GVTvjUZYMx4borp84541HuApaJ/ZLyJI55ScQK0L5IaTTwuDGRNumaARTt/U8+JA1sXxybZU++NA2BVm0R1xKNaAsSs5pvY6K0aaE418k0zP6xsxjXciFnMR8o3FID/dv/Lnn2jgaVfAmC+qQDMcPiDI13DpwBmQ+Tr1MByzTZmWrSYlPa1WotOAIgnmj6kfEtKgJGpX+czC90zgAmpX+s1aAPAIwMAIxzxjMoaHA03DY2erVOAiSMfAIAAwCwtQKFQ6CL6Z9LAOABkA9g8ODjowfyn1ev1LlzXrRoHgOz/oslkMgqLp2v4XqdIJPI6m82+MLEEDP9ZNACYouPj48/pdLpg9BgIPaquTPn1YrGYNf4cAHoo3/drl5eXIkjzMQZqKs/zTjnnL2W9D7wsAgDNVC6XPyUSife2nQNAcr4/PDysbG1trfRdiegBNJAQ4nelUrnZ29tb+bsSAUDI6nQ6ZxKApIz8UA7BAEDIaT8Wi4VahtEDhDzthf0BAIDlAgAAAAIAEACA7NTcY2Amk4mUSqUGtk6Jbra3tzdJATBU2B8aQgmAAAAEACAAAAEAq6T6zTkAgJiur687AMBieZ73TeX1GN7GRUv9fv+H4zhZlRkAf45FRF+kVJqPEkBIruue7u7uflB9XTZRAVAONFS73T6Lx+Nvg7j2XwEGACbouxHl2VRVAAAAAElFTkSuQmCC"
+        };
 
-    glue.windows.my().onFrameButtonClicked((buttonInfo) => {
+        // TUTOR_TODO Chapter 4.4 Task 7
+        // Implement the frame button click events:
+        // - Which button was clicked?
+        // - How are we going to remember the tabs we detached?
+        // - glue.windows.findById() will be quite helpful here
 
-    });
+        thisPortfolioWindow.onFrameButtonClicked((button) => {
+            switch (button.buttonId) {
+                case "gatherTabs": gatherTabs(); break;
+                case "extractTabs": extractTabs(); break;
+                default: break;
+            }
+        });
 
-    // TUTOR_TODO Chapter 4.4 Task 8
-    // What frame button should be displayed when the tab is created?
-    // Implement the logic for each of these events - which buttons should show/hide in each scenario?
-    // hint - maybe glue.windows.my().tabs.length would be useful somewhere?
+        const gatherTabs = () => {
+            const tabsToGather = clientWindow.context.extractedTabIDs;
 
-    glue.windows.my().onWindowAttached((win) => {
+            // attach all scattered tabs to the current tab
+            tabsToGather.forEach(tabID => {
+                const tab = glue.windows.findById(tabID);
 
-    });
+                thisPortfolioWindow.attachTab(tab).then(tab => {
+                        console.log(`Tab with ID ${tab.id} was attached.`);
+                    }).catch(error => {
+                        console.error(error.message)
+                    });
+            });
 
-    glue.windows.my().onAttached((win) => {
+            // snap the current tab to the Clients window
+            const direction = getWindowDirection();
 
-    });
+            thisPortfolioWindow.snap(clientWindow, direction)
+                .then(() => {
+                    console.log(`Portfolio window with ID ${thisPortfolioWindow} was snapped to the Clients window.`)
+                }).catch(error => {
+                    console.error(error);
+                });
+        };
 
-    glue.windows.my().onDetached((win) => {
+        const extractTabs = () => {
+            const tabsToExtract = thisPortfolioWindow.tabs;
+            // this way we will always have the current number of tabs from the Portfolio tab group
+            // even if the user at the time of pressing the Gather Tabs button has previously extracted them,
+            // closed some of them, or opened new ones, combined some of them, etc.
+            const extractedTabIDs = glue.windows.list().map(tab => { 
+                if (tab.tabGroup && tab.tabGroup === portfolioTabGroupID) {
+                    return tab.id; 
+                }
+            });
+            const context = { extractedTabIDs: extractedTabIDs };
 
-    });
+            clientWindow.updateContext(context);
 
-    glue.windows.my().onWindowDetached((win) => {
+            tabsToExtract.forEach(tab => tab.detachTab().then(() => {
+                    console.log("The tab was detached.")
+                }).catch(error => {
+                    console.error(error.message)
+                }));
+        };
 
-    });
+        const getWindowDirection = () => {
+        
+            const primaryMonitor = glue42gd.monitors.find(monitor => monitor.isPrimary === true);
+            const bottomNeighbors = clientWindow.bottomNeighbours;
+            const availableSpaceBelow = primaryMonitor.workingAreaHeight - (clientWindow.bounds.top + clientWindow.bounds.height);
+        
+            // if there are no bottom heighbors and there is available space, open the Portfolio below the Clients, otherwise - to the right
+            return bottomNeighbors.length === 0 && availableSpaceBelow >= thisPortfolioWindow.bounds.height ? "bottom": "right";
+        }
 
+        // TUTOR_TODO Chapter 4.4 Task 8
+        // What frame button should be displayed when the tab is created?
+        // Implement the logic for each of these events - which buttons should show/hide in each scenario?
+        // hint - maybe glue.windows.my().tabs.length would be useful somewhere?
+
+        // handle button changes on the window to which tabs are being attached/detached
+        thisPortfolioWindow.onWindowAttached(() => {
+            setupFrameButtons(gatherTabsBtn, extractTabsBtn);
+        });
+
+        thisPortfolioWindow.onWindowDetached(() => {
+            if (thisPortfolioWindow.tabs.length === 2) {
+                setupFrameButtons(extractTabsBtn, gatherTabsBtn);
+            }
+        });
+
+        // handle button changes on the windows which are being attached/detached to another window
+        thisPortfolioWindow.onAttached(() => {
+            setupFrameButtons(gatherTabsBtn, extractTabsBtn);
+        });
+
+        thisPortfolioWindow.onDetached(() => {
+            setupFrameButtons(extractTabsBtn, gatherTabsBtn);
+        });
+
+        // set up frame buttons when scattering/gathering tabs
+        const setupFrameButtons = (buttonToRemove, buttonToAdd) => {
+            let buttonToRemoveExists;
+            let buttonToAddExists;
+
+            if (buttonToRemove && typeof buttonToRemove === "object") {
+
+                buttonToRemoveExists = checkIfButtonExists(buttonToRemove.buttonId);
+
+                if (buttonToRemoveExists) {
+                    thisPortfolioWindow.removeFrameButton(buttonToRemove.buttonId)
+                        .then(() => {
+                            console.log(`${buttonToRemove.buttonId} button successfully removed.`);
+                        }).catch(error => {
+                            console.error(error.message);
+                        });
+                }
+            }
+
+            if (buttonToAdd && typeof buttonToAdd === "object") {
+
+                buttonToAddExists = checkIfButtonExists(buttonToAdd.buttonId);
+
+                if (!buttonToAddExists) {
+                    thisPortfolioWindow.addFrameButton(buttonToAdd)
+                        .then(() => {
+                            console.log(`${buttonToAdd.buttonId} button successfully added.`);
+                        }).catch(error => {
+                            console.error(error.message);
+                        });
+                }
+            }
+        };
+
+        const checkIfButtonExists = (buttonID) => {
+            const doesButtonExist = 
+                thisPortfolioWindow.frameButtons
+                .find(({ buttonId }) => buttonId === buttonID) !== undefined ? true : false;
+
+            return doesButtonExist;
+        }
+    }
 };
 
 const search = (event) => {
